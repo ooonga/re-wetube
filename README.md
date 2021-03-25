f76903bd82c3315a06ca2e898f34f66e80a5d6a6
ooonga <2ragu@naver.com>
2021-03-25 13:12:42 +0900

    views - editProfile,join,login,search..

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WETUBE
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
 import routes from "../routes";
+import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/", users);
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.changePassword, changePassword);
 
 
